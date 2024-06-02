c9af0a0693fcf0fc79a194416eef95438d7de40f
LiamAcker <allschwrtz849@gmail.com>
2024-06-02 19:44:51 +0700

    added calendar controller

--- plansync-app/routes/web.php
+++ plansync-app/routes/web.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use app\Http\Controllers\UserController;
+use app\Http\Controllers\CalendarController;
 
 
 # User Routes
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 
-
+Route::get('/calendar', [CalendarController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
