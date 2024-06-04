d7673493d98e053a585f2f8edfaa9137a17b939f
LiamAcker <allschwrtz849@gmail.com>
2024-06-04 11:21:12 +0700

    a lot of changes

--- plansync-app/app/Console/Commands/SendEmail.php
+++ plansync-app/app/Console/Commands/SendEmail.php
@@ -3,8 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Models\Reminder;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
+use App\Mail\OrderMail;
 class SendEmail extends Command
 {
     /**
@@ -26,22 +29,43 @@ class SendEmail extends Command
      */
     public function handle()
     {
-        $current_datetime = Carbon::now();
+
+        $current_datetime = Carbon::now()->startOfMinute();
+        $reminders = Reminder::where('remind_datetime', $current_datetime)
+        ->get();
+
+        echo("Number of reminders: ". count($reminders));
+    
+        // foreach($reminders as $reminder){
+
+        //     Mail::send('emails.view', ['data' => $reminder], function($message) use ($reminder) {
+        //         $message->to($reminder->email, $reminder->name)
+        //                 ->subject('Your Reminder: ' , $reminder->title_string);
+        //     });
         
-        $reminders = Reminder::find();
-        //
-        $data = [
-            // Your email data here
+        //     $this->info('Email has been sent!');
+        // }
+
+          
+      
+
+        $reminder = [
+            'email' => "linggartn2@gmail.com",
+            'name' => "Linggar Nusantara",
+            'title_string' => "The title",
+            'description_string' => "The description"
         ];
-
-        Mail::send('emails.view', $data, function($message) {
-            $message->to('example@example.com', 'Recipient Name')
-                    ->subject('Your Email Subject');
-        });
-
+        
+        
+        $reminder = Reminder::find($id);
+        Mail::to($user->email)->send(new OrderMail($reminder, $user));
+    
         $this->info('Email has been sent!');
+        
+        
     }
+        //
+
 
         
     }
-}
